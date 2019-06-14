262197ba72550cd14f27e37a9a3cbdd701103b39
Jonathan Kelvin <j_kelvin1997@hotmail.com>
2019-06-13 21:35:11 -0300

    Paginação com parametros opcionais na requisição

--- src/main/java/com/jonkelvin/cursomc/CursomcApplication.java
+++ src/main/java/com/jonkelvin/cursomc/CursomcApplication.java
@@ -62,6 +62,10 @@ public class CursomcApplication implements CommandLineRunner {
 
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritorio");
+		Categoria cat3 = new Categoria(null, "Quarto");
+		Categoria cat4 = new Categoria(null, "Jardinagem");
+		Categoria cat5 = new Categoria(null, "Lazer");
+		Categoria cat6 = new Categoria(null, "Arquitetura");
 		
 		Produto p1 = new Produto(null, "Computador", 2000.0);
 		Produto p2 = new Produto(null, "Impressora", 800.0);
@@ -74,7 +78,7 @@ public class CursomcApplication implements CommandLineRunner {
 		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
 		p3.getCategorias().addAll(Arrays.asList(cat1));
 
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
 
 		Estado s1 = new Estado(null, "Minas Gerais");
