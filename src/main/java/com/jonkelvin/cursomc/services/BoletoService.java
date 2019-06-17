2844ba5881c7912000cc53436a8efab0b86a04ed
Jonathan Kelvin <jonathan.santos@dcomp.ufs.br>
2019-06-17 02:01:24 -0300

    Inserindo Pedido

---
+++ src/main/java/com/jonkelvin/cursomc/services/BoletoService.java
@@ -0,0 +1,19 @@
+package com.jonkelvin.cursomc.services;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.stereotype.Service;
+
+import com.jonkelvin.cursomc.domain.PagamentoComBoleto;
+
+@Service
+public class BoletoService {
+
+	public void preencherPagamentoComBoleto(PagamentoComBoleto pagto, Date instanteDoPedido) {
+		Calendar cal = Calendar.getInstance();
+		cal.setTime(instanteDoPedido);
+		cal.add(Calendar.DAY_OF_MONTH, 7);
+		pagto.setDataVencimento(cal.getTime());
+	}
+}
